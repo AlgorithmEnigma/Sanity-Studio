c8573e30fe5fe6c873e576047577bc07042e5b9c
Jordan Lowell <wraithbournejl@gmail.com>
2022-08-13 23:27:10 -0400

    first commit

---
+++ schemas/tech.js
@@ -0,0 +1,32 @@
+export default {
+  name: 'tech',
+  title: 'Tech',
+  type: 'document',
+  fields: [
+    {
+        name: 'title',
+        title: 'Title',
+        type: 'string',
+    },
+    {
+        name: 'description',
+        title: 'Description',
+        type: 'text',
+    },
+    {
+        name: 'icon',
+        title: 'Icon',
+        type: 'url',
+      },
+    {
+        name: 'repo',
+        title: 'Repo',
+        type: 'url',
+      },
+    {
+        name: 'website',
+        title: 'Website',
+        type: 'url',
+    }
+  ],
+}
